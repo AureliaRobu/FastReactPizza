caba52c7f46051bb0eb41e88fbb484fc86fdc9c6
Aurelia Robu <robuaurelia@gmail.com>
2023-12-04 09:49:03 +0200

    Loader an error handling

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,23 @@ import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
-import AppLayout from "./ui/AppLayout.tsx";
+import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
 
 const router = createBrowserRouter([
   {
-    element: <AppLayout/>,
-    children: [{
-      path: '/',
-      element: <Home />,
-    },
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
       {
         path: '/menu',
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: '/cart',
@@ -29,9 +33,9 @@ const router = createBrowserRouter([
       {
         path: '/order/:orderId',
         element: <Order />,
-      }]
-  }
-
+      },
+    ],
+  },
 ]);
 
 export default function App() {
